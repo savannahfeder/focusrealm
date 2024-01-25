e9c1b18efc01c45b6aa1bf39943a017bd00be223
Savannah <sfeder0@gmail.com>
2024-01-24 16:06:50 -0800

    added task overflow scroll bar

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -6,7 +6,7 @@ const Tasks = ({ tasks, deleteTask }) => {
   return (
     <div>
       <StyledHeader text="Tasks" />
-      <div className="bg-off-white-color bg-opacity-70 w-[350px] h-[285px] rounded-3xl px-4 pt-6">
+      <div className="bg-off-white-color bg-opacity-70 w-[350px] h-[285px] rounded-3xl px-4 pt-6 overflow-auto">
         {tasks.map((task) => (
           <Task key={task.id} task={task} deleteTask={deleteTask} />
         ))}
